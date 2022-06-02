19655fe6d6c5ec933640828765521d6c5c716c19
KoyFoster <12465882+KoyFoster@users.noreply.github.com>
2022-06-02 14:12:32 -0500

    Cleared white html bg

--- src/App.tsx
+++ src/App.tsx
@@ -16,17 +16,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Canvas id='canvas' width={1280} height={720}></Canvas>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
