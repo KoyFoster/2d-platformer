5cd328f2883cf38d62c82d1003c58a515970f2e4
KoyFoster <12465882+KoyFoster@users.noreply.github.com>
2022-06-05 21:28:41 -0500

    Seperated entites between fore and backgrounds

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -10,7 +10,8 @@ const randomcolor = require('randomcolor');
 export default class Game {
     private canvas = document.querySelector('canvas') as HTMLCanvasElement;
     private ctx = this.canvas.getContext('2d')!; // The '!' tells TS that the context always exists
-    private platforms = [] as Entity[];
+    private backEntities = [] as Entity[];
+    private foreEntities = [] as Entity[];
     private frame = [] as Entity[];
     private camera = { x: -this.canvas.width * 0.5, y: -this.canvas.height * 0.75, z: 0 };
     private player = new Player({ x: 0, y: 24, z: 0 });
@@ -28,7 +29,7 @@ export default class Game {
     private dev = new DevTools(this.camera);
 
     public constructor() {
-        this.cage.setAnchor({ x: 0.5, y: 0, z: 0 })
+        this.cage.setAnchor({ x: 0.5, y: 1, z: 0 })
 
         // Input Listeners
         window.addEventListener('keydown', (e) => this.onKey(e, true));
@@ -46,23 +47,27 @@ export default class Game {
                 case "Platform":
                     pl = new Platform(e.pos, e.size, e.color);
                     pl.setAnchor(e.anchor);
+                    pl!.setVel(e.vel);
+                    this.foreEntities.push(pl);
                     break;
                 case "tractor":
                     pl = new Tractor(e.pos, e.size, e.color);
                     pl.setAnchor(e.anchor);
+                    pl!.setVel(e.vel);
+                    this.foreEntities.push(pl);
                     break;
                 case "motion":
                     pl = new HurtBox_Motion(e.pos, e.size, e.color);
                     pl.setAnchor(e.anchor);
+                    pl!.setVel(e.vel);
+                    this.backEntities.push(pl);
                     break;
                 default:
                     pl = new HurtBox(e.pos, e.size, e.color);
                     pl.setAnchor(e.anchor);
+                    pl!.setVel(e.vel);
+                    this.backEntities.push(pl);
                     break;
-            }
-            if (pl !== null) {
-                pl!.setVel(e.vel);
-                this.platforms.push(pl);
             }
         });
     }
@@ -70,12 +75,12 @@ export default class Game {
     public loadSequences(sequences: any) {
         // load cage
         this.cage = new Cage(cages[sequences.cage][0], cages[sequences.cage][1], 'white');
-        this.cage.setAnchor({ x: 0.5, y: 0, z: 0 })
+        this.cage.setAnchor({ x: 0.5, y: 1, z: 0 })
 
         // Set deallocation timer      
         const { lifetime } = sequences;
         const interval = setInterval(() => {
-            this.platforms = [];
+            this.backEntities = [];
             clearInterval(interval);
         }, lifetime);
 
@@ -84,19 +89,8 @@ export default class Game {
         });
     }
 
-    public loadMap(map: Vector[][]) {
-        map.forEach(row => {
-            this.platforms.push(new Platform(row[0], row[1], 'white'));
-        });
-
-        // Test HurtBox
-        const pl = new HurtBox({ x: -500, y: 0, z: 0 }, { x: 20, y: 100, z: 0 }, 'green');
-        pl.setVel({ x: __speed__, y: 0, z: 0 });
-        this.platforms.push(pl);
-    }
-
     debug() {
-        this.ctx!.fillText(`Entities:${this.platforms.length}`, 33, 110);
+        this.ctx!.fillText(`Entities:${this.backEntities.length}`, 33, 110);
     }
 
     // Game Logic
@@ -107,40 +101,62 @@ export default class Game {
         this.ctx!.fillStyle = "#000000";
         this.ctx!.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
-        // platforms
-        this.platforms.forEach(entity => {
+        // A) LOGIC
+        // backEntities
+        this.backEntities.forEach(entity => {
             entity.tick(null, delta);
-            entity.draw(this.ctx, this.camera);
+        });
+        this.foreEntities.forEach(entity => {
+            entity.tick(null, delta);
         });
 
         const jump = this.keys.jump;
         const jumphold = (this.keys.jump && this.prevKeys.jump);
         this.player.move(this.keys.left, this.keys.right, jump, jumphold, delta);
-        this.player.tick(this.platforms, delta);
+        this.player.tick([...this.backEntities, ...this.foreEntities], delta);
         // keep player insize cage
         this.cage.affect(this.player);
-        this.cage.draw(this.ctx, this.camera);
 
         // do to player player velocity being calculated all throughout prior to this
         // we need to calculate platform affects in post in order for motion based 
         // affects to apply properly
         // check affects
-        this.platforms.forEach(platform => {
-            if (this.player.checkCollision(platform)) {
+        this.backEntities.forEach(entity => {
+            if (this.player.checkCollision(entity)) {
                 // Done last to ensure that the final velocity affects are calculated
                 // perform entity affect on player
-                platform.affect(this.player, delta);
+                entity.affect(this.player, delta);
+            }
+        });
+        this.foreEntities.forEach(entity => {
+            if (this.player.checkCollision(entity)) {
+                entity.affect(this.player, delta);
             }
         });
 
-        this.player.draw(this.ctx, this.camera);
-        this.player.UI(this.ctx);
-        this.player.debug(this.ctx);
-
         // remember previous key presses
         this.prevKeys.jump = this.keys.jump;
-
         this.dev.tick(this.ctx, this.camera, delta);
+        // END OF LOGIC
+
+        // B) DRAW
+        // player should show behind the objects
+        this.player.draw(this.ctx, this.camera);
+        // everything should not show outside the cage, unless they are intelligent objects, like backEntities and blasters
+        this.backEntities.forEach(entity => {
+            entity.draw(this.ctx, this.camera);
+        });
+        // hard coded rectangles outside of cage        
+        this.ctx!.fillStyle = "#000000";
+        this.ctx!.fillRect(-150, 350, this.canvas.width * 0.33, this.canvas.height * 0.4);
+        this.ctx!.fillRect(1010, 350, this.canvas.width * 0.33, this.canvas.height * 0.4);
+        this.cage.draw(this.ctx, this.camera);
+        this.foreEntities.forEach(entity => {
+            entity.draw(this.ctx, this.camera);
+        });
+
+        this.player.UI(this.ctx);
+        this.player.debug(this.ctx);
 
         // UI
         // draw relative to camera
@@ -174,17 +190,5 @@ export default class Game {
                 break;
             default: return;
         }
-    }
-
-    addEntity(size: Vector, pos: Vector, color: string) {
-        this.platforms.push(new Platform(pos, size, color));
-    }
-
-    addRandomEntity() {
-        const pos = { x: 20, y: 20, z: 0 };
-        const size = { x: 20, y: 20, z: 20 };
-        const color = randomcolor();
-
-        this.addEntity(pos, size, color);
     }
 }
